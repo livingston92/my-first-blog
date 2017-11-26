6d032e05653d1848e88d297d5a43ace73643247b
livingston <kovalchukbogdan01@gmail.com>
2017-11-26 23:15:42 +0100

    My Django Girls app, first commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/livingston/djangogirls/myvenv/bin/python3
+# EASY-INSTALL-SCRIPT: 'Django==1.11.7','django-admin.py'
+__requires__ = 'Django==1.11.7'
+__import__('pkg_resources').run_script('Django==1.11.7', 'django-admin.py')
